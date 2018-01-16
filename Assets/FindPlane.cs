1db912806c78176ac8e9418dae814519bea2eac6
sandra <sdang@kth.se>
2018-01-16 14:28:08 +0100

    Initial commit

---
+++ Assets/FindPlane.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindPlane : MonoBehaviour
+{
+
+	public GameObject moose;
+	private GameObject planex;
+
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+		if (planex == null) {
+			planex = GameObject.Find ("Plane");
+			Vector3 pos = planex.transform.position;
+			GameObject Moose;
+			Moose = Instantiate (moose, pos, Quaternion.identity) as GameObject;
+			Moose.SetActive (true);
+		}
+	}
+}
